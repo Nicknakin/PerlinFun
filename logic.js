90f0fdeffa5533cf21b53eeb9d55a69fb438d424
Nick Kinney <Nick.Kinney@sabre.com>
2019-05-29 10:19:49 -0500

    A few edits

--- logic.js
+++ logic.js
@@ -4,10 +4,10 @@ let xOffset, yOffset;
 let xOffsetStart, yOffsetStart;
 let xInc, yInc;
 let time = 0;
-let dims = [1, 1];
+let dims = [8, 8];
 let balls = [];
 let numBalls = 2500;
-let maxSpeed = 5;
+let maxSpeed = 4;
 
 let currentX, currentY;
 
@@ -25,23 +25,19 @@ function setup(){
         currentX %= width;
         if(currentX == 0) currentY = (currentY+1)%height;
     }
-    noiseDetail(16);
+    noiseDetail(32);
+    background(0);
 }
 
 function draw(){
-    fill(0, 0, 0, 8);
-    rect(0,0,width,height);
     balls.forEach((ball) => {
         const noiseX = ball.pos.x/dims[0]*xInc+xOffsetStart;
         const noiseY = ball.pos.y/dims[1]*yInc+yOffsetStart;
-        const noiseNum = noise(noiseX, noiseY, time);
+        const noiseNum = noise(noiseX, noiseY);
         const noiseAngle = map(noiseNum, 0, 1, -2*PI, 2*PI);
         const noiseVector = p5.Vector.fromAngle(noiseAngle);
-        const distFromPoint1 = ball.pos.copy().sub(width/4, height/2);
-        const distFromPoint2 = ball.pos.copy().sub(3*width/4, height/2);
-        // if(distFromPoint1.mag() == 0 || distFromPoint2.mag() == 0) ball.antiBall = !ball.antiBall;
-        ball.c.r = ball.c.g = ball.c.b = map(noiseNum,0,1,64,255);
-        ball.a = distFromPoint1.normalize().mult(-1).add(distFromPoint2.normalize()).add(noiseVector);
+        ball.c.r = ball.c.g = ball.c.b = map(noise(noiseAngle, time), 0, 1, 0, 255);
+        ball.v = (noiseVector);
         if(ball.v.mag() > maxSpeed){
             ball.v = ball.v.normalize();
             ball.v = ball.v.mult(maxSpeed);
@@ -53,5 +49,6 @@ function draw(){
             ball.v = ball.v.mult(-1);
         }
     });
-    time += 0.0005;
+
+    time += 0.005;
 }
